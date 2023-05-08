cf7cd0163afbf366df96fbfe43dc0b395997c30d
LUIGI GUIMARAES TREVISAN <22.01102-0@maua.br>
2023-05-08 08:44:52 -0300

    tests

--- test_methods.py
+++ test_methods.py
@@ -21,3 +21,47 @@ def test_perimeter():
     
     # then the perimeter should be 14
     assert output == 14
+    
+def test_sum():
+    # given two numbers
+    num1 = 2
+    num2 = 5
+    
+    # when we calculate the sum
+    output = methods.sum(num1, num2)
+    
+    # then the sum should be 7
+    assert output == 7
+    
+def test_difference():
+    # given two numbers
+    num1 = 2
+    num2 = 5
+    
+    # when we calculate the difference
+    output = methods.difference(num1, num2)
+    
+    # then the difference should be -3
+    assert output == -3
+    
+def test_multiplication():
+    # given two numbers
+    num1 = 2
+    num2 = 5
+    
+    # when we calculate the multiplication
+    output = methods.multiplication(num1, num2)
+    
+    # then the multiplication should be 10
+    assert output == 10
+    
+def test_division():
+    # given two numbers
+    num1 = 2
+    num2 = 5
+    
+    # when we calculate the division
+    output = methods.quotient(num1, num2)
+    
+    # then the division should be 0.4
+    assert output == 0.4    
